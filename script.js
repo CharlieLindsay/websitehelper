1912b520424cd1a6fc26381d90c4835377cfc2d8
Charles Lindsay <186342@cloud.rangitoto.school.nz>
2026-06-23 20:34:30 +1200

    added weather and time

--- script.js
+++ script.js
@@ -25,14 +25,23 @@ if (!localStorage.getItem("second_link")) {
 if (!localStorage.getItem("second_name")) {
     localStorage.setItem("second_name", "Stardance");
 }
+if (!localStorage.getItem("third_link")) {
+    localStorage.setItem("third_link", "https://google.com");
+}
+if (!localStorage.getItem("third_name")) {
+    localStorage.setItem("third_name", "Google Search");
+}
 
 let first_name = localStorage.getItem("first_name");
 let first_link = localStorage.getItem("first_link");
-let second_name = localStorage.getItem("first_name");
-let second_link = localStorage.getItem("first_link");
+let second_name = localStorage.getItem("second_name");
+let second_link = localStorage.getItem("second_link");
+let third_name = localStorage.getItem("third_name");
+let third_link = localStorage.getItem("third_link");
 
 document.getElementById("first_button").value = first_name;
 document.getElementById("second_button").value = second_name;
+document.getElementById("third_button").value = third_name;
 
 function first_click() {
     window.open(first_link, "_blank");
@@ -73,4 +82,25 @@ function second_edit() {
 
 
     document.getElementById("second_button").value = second_name;
+}
+
+
+function third_click() {
+    window.open(third_link, "_blank");
+}
+
+function third_edit() {
+    let newName = prompt("Enter name:", third_name);
+    let newLink = prompt("Enter link:", third_link);
+    newLink = normalizeUrl(newLink);
+    if (!newName || !newLink) return;
+
+    localStorage.setItem("third_name", newName);
+    localStorage.setItem("third_link", newLink);
+
+    third_name = newName;
+    third_link = newLink;
+
+
+    document.getElementById("third_button").value = third_name;
 }
